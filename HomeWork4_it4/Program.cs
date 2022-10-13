02b4208451cc9cf63383f45fd452702aa68ca025
David <ladbl1971@gmail.com>
2022-10-13 13:42:16 +0400

    First Homework

---
+++ HomeWork4_it4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork4_it4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
